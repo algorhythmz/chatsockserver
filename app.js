547d1b18c24975822d1cc90272c837cc6f0ab053
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-09-13 11:16:29 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Brekkishhh on 11-08-2016.
+ */
+
+var http = require('http').createServer(serverHandler);
+var io = require('socket.io')(http);
+
+http.listen(3000);
+
+
+
+function serverHandler(req,res) {
+    res.end('Socket Server is running on Port 3000');
+}
+
+io.on('connection',function (socket) {
+    console.log('server is running');
+
+    socket.on('update',function (data) {
+        console.log(data);
+        socket.emit('response','Hey i am main server');
+    });
+});
